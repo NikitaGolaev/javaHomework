609719553758969ba30981890c68e35c7269d20d
NikitaGolaev <98217201+NikitaGolaev@users.noreply.github.com>
2022-06-27 19:00:04 +0300

    added task_1

---
+++ .idea/src/GB/task/task_1_hm.java
@@ -0,0 +1,10 @@
+package GB.other;
+
+public class sqrt {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 2;
+        System.out.println( (int) Math.pow(x, y));
+    }
+}
